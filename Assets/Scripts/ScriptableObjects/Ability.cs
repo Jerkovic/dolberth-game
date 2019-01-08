675c18716858877a17b987c41db436a606def383
erikl <erilundin@gmail.com>
2019-01-08 21:37:58 +0100

    Added basic abilities

---
+++ Assets/Scripts/ScriptableObjects/Ability.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public abstract class Ability : ScriptableObject
+{
+
+    public string abilityName = "Abstract Ability";
+    public float baseCoolDown = 5f;
+
+    public abstract void Initialize(GameObject obj);
+    public abstract void TriggerAbility();
+}
---
+++ Assets/Scripts/ScriptableObjects/ShootAbility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilities/Shoot")]
+public class ShootAbility : Ability
+{
+
+    public int gunDamage = 1;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Color laserColor = Color.white;
+
+    public override void Initialize(GameObject obj)
+    {
+
+    }
+
+    public override void TriggerAbility()
+    {
+        //rcShoot.Fire();
+    }
+
+
+}
